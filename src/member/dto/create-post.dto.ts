a2ed7aec55f157bf359c1bd84880d171130f09f3
chandima <chandimaofficial@gmail.com>
2021-06-09 21:25:28 +0530

    created member dto

---
+++ src/member/dto/create-post.dto.ts
@@ -0,0 +1,7 @@
+export class CreatePostDTO {
+    readonly name: string;
+    readonly email: string;
+    readonly mobile: string;
+    readonly address: string;
+    readonly date_posted: string;
+  }
