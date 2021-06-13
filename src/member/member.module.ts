2d76206a1e943156cf4b369d829331a4cc4439f4
chandima <chandimaofficial@gmail.com>
2021-06-13 18:51:52 +0530

    added AUTH0 middleware - success

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
-import { MongooseModule } from '@nestjs/mongoose'; // add this
-import { MemberSchema } from './schemas/member.schema'; // and this
+import { MongooseModule } from '@nestjs/mongoose';
+import { MemberSchema } from './schemas/member.schema'; 
+import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
     
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
-    ], // add 
+    ], 
   providers: [MemberService],
   controllers: [MemberController],
 })
-export class MemberModule {}
+export class MemberModule  implements NestModule {
+
+  //auth0 middleware 
+    configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+      consumer.apply(AuthenticationMiddleware).forRoutes(
+        { method: RequestMethod.POST, path: '/member/create' },
+        { method: RequestMethod.GET, path: '/member' },
+        { method: RequestMethod.GET, path: '/member/:memberID' },
+        { method: RequestMethod.PUT, path: '/member/edit/:memberID' },
+        { method: RequestMethod.DELETE, path: '/member/delete/:memberID' }
+      )
+    }
+}
